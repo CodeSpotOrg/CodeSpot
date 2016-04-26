6df37056983140f3c24341590a82f14cb795295a
Steve McLaughlin <stevejrmc@gmail.com>
2016-04-26 15:03:24 -0700

    updated places route to start handling search criteria and changed form to get in partials

--- routes/places.js
+++ routes/places.js
@@ -1,8 +1,14 @@
+'use strict'
 const express = require('express'),
       router = express.Router({mergeParams:true});
 
-router.get('/', function(req,res){
-	res.render('site_views/index.jade');
+
+router.get('/',(req,res)=>{
+	console.log('Query:',req.query);
+	res.render('site_views/index');
 });
 
+// router.get('/',(req,res)=>{
+// 	res.render('site_views/index.jade');
+// });
 module.exports = router;
