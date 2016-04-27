d2a94a1549ce5d58ed5894b97979bb7dcc4a5c2f
Steve McLaughlin <stevejrmc@gmail.com>
2016-04-27 16:51:17 -0700

    added knex to reviews.js file and submit button to the reviews partials form

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require('express'),
       router = express.Router({mergeParams:true}),
-      helpers = require('../helpers/authHelpers');
+      helpers = require('../helpers/authHelpers'),
+      knex = require('../db/knex');
 
 
 router.get('/new',helpers.ensureAuthenticated,(req,res)=>{
